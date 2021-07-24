f33e57f1a8ad4053965ba4f78eae20794c660632
Dogedox <moefromsimpsonss@gmail.com>
2021-07-24 18:11:21 +0300

    added 'build' to express.static

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const app = express()
 
 app.use(express.json())
-app.use(express.static())
+app.use(express.static('build'))
 app.use(cors())
 app.use(morgan( (tokens, req , res) =>{
     morgan.token('body', (req) => {return JSON.stringify(req.body)})
