84c32c3ae6d5b5f6864decf978f9cdcf764fc21e
Dogedox <moefromsimpsonss@gmail.com>
2021-07-24 17:32:19 +0300

    profile and port fix

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ app.delete('/api/persons/:id', (req,res) =>{
 
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT,() =>{
-    console.log(`running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
