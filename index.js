30d3ce7d0a29e580f67df6846f2e0c354cba0ca1
Dogedox <moefromsimpsonss@gmail.com>
2021-07-24 18:06:43 +0300

    builded front and static middleware added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(express.static())
 app.use(cors())
 app.use(morgan( (tokens, req , res) =>{
     morgan.token('body', (req) => {return JSON.stringify(req.body)})
