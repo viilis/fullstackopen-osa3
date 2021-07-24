c7be2d32a1de6541cb96e20dd6cdc5465a190b21
Dogedox <moefromsimpsonss@gmail.com>
2021-07-24 21:51:00 +0300

    3.12 done

--- mongo.js
+++ mongo.js
@@ -4,19 +4,31 @@ mongoose.connect(
     `mongodb+srv://dogedox:${process.argv[2]}@fso-cluster.imy7u.mongodb.net/fso-phonebook?retryWrites=true&w=majority`,
     {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true}
 )
+
 const Person = mongoose.model(
     'Person', new mongoose.Schema({
         name: String,
         number: String,
     })
 )
+if(process.argv.length<=3){
+    Person.find({}).then(res =>{
+        res.forEach(p =>{
+            console.log(p)
+        })
+        mongoose.connect.close()
+    })
+}
+
+const personname = process.argv[3]
+const personnumber = process.argv[4]
 
 const person = new Person({
-    name: process.argv[3],
-    number: process.argv[4],
+    name: personname,
+    number: personnumber,
 })
 
 person.save().then(res => {
-    console.log('saved')
+    console.log(`added ${personname} number ${personnumber} to phonebook`)
     mongoose.connection.close()
 })
